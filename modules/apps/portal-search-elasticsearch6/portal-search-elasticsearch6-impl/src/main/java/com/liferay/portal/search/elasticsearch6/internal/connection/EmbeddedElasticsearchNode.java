e3fe11d415c80f4b7f1904d70c4b9cea21833bf0
Bryan Engler <bryan.engler@liferay.com>
2018-11-02 11:50:03 -0700

    LPS-86861 portal-search-elasticsearch6-impl: Elasticsearch 6.4.3: Java API changes
    https://issues.liferay.com/browse/LPS-86861

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/connection/EmbeddedElasticsearchNode.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/connection/EmbeddedElasticsearchNode.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.elasticsearch.analysis.common.CommonAnalysisPlugin;
 import org.elasticsearch.common.logging.LogConfigurator;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
@@ -40,7 +41,8 @@ public class EmbeddedElasticsearchNode extends Node {
 			settings, null);
 
 		List<Class<? extends Plugin>> classpathPlugins = Arrays.asList(
-			Netty4Plugin.class, ReindexPlugin.class);
+			CommonAnalysisPlugin.class, Netty4Plugin.class,
+			ReindexPlugin.class);
 
 		try {
 			LogConfigurator.configure(environment);
