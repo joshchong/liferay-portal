d0afa10eae3cb5acd4f0299408d3c718d1368df5
Joshua Chong <joshchong@gmail.com>
2020-02-07 13:14:56 -0800

    LPS-107010 Auto SF

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/resources/META-INF/resources/js/components/ResultRankingsForm.es.js
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/resources/META-INF/resources/js/components/ResultRankingsForm.es.js
@@ -475,8 +475,7 @@ class ResultRankingsForm extends Component {
 						type: 'danger'
 					});
 				});
-			}
-			else {
+			} else {
 				this.setState(
 					{
 						workflowAction: this.context.constants
